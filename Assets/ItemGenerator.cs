b92e8ff1a373062679532b1378873b157cad4a89
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-07-15 22:49:08 -0500

    added item database

---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public TextMeshProUGUI itemName;
+    public TextMeshProUGUI itemInfo;
+    public TextMeshProUGUI extraStat;
+    public TextMeshProUGUI target;
+
+    [Space]
+    public TextMeshProUGUI Power;
+    public TextMeshProUGUI Accuracy;
+    public TextMeshProUGUI Constituion;
+    public TextMeshProUGUI Speed;
+    public TextMeshProUGUI Luck;
+
+    public Image icon;
+
+    public void GenerateItem()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Weapons> weapons;
+    public List<Armors> armors;
+}
+
+[System.Serializable]
+public class Weapons
+{
+    public WeaponTypes weaponType;
+    public List<Sprite> weaponIcons;
+}
+
+[System.Serializable]
+public class Armors
+{
+    public ArmorTypes armorTypes;
+    public List<Sprite> armorIcons;
+}
+
+public enum WeaponTypes
+{
+    Axe,
+    Bow,
+    Crossbow,
+    Dagger,
+    FistWeapon,
+    Focus,
+    Gun,
+    Mace,
+    Scythe,
+    Shield,
+    Spear,
+    Staff,
+    Sword,
+    Tome,
+    TwoHandedAxe,
+    TwoHandedMace,
+    TwoHandedSword
+}
+public enum ArmorTypes
+{
+    LightBelt,
+    LightBoots,
+    LightBracers,
+    LightChectplate,
+    LightCloak,
+    LightGloves,
+    LightHelmet,
+    LightLegplate,
+    LightShoulderpads,
+
+    MediumBelt,
+    MediumBoots,
+    MediumBracers,
+    MediumChectplate,
+    MediumCloak,
+    MediumGloves,
+    MediumHelmet,
+    MediumLegplate,
+    MediumShoulderpads,
+
+    HeavyBelt,
+    HeavyBoots,
+    HeavyBracers,
+    HeavyChectplate,
+    HeavyGloves,
+    HeavyHelmet,
+    HeavyLegplate,
+    HeavyShoulderpads,
+
+    Necklace,
+    Rings
+}
